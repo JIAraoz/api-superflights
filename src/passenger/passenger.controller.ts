718970c92a26daa5a4ddbb9c3a8319a48719a782
Nachvander <nachvander@gmail.com>
2024-09-05 17:49:01 -0300

    feat-createPassenger

--- src/passenger/passenger.controller.ts
+++ src/passenger/passenger.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { PassengerDTO } from './dto/passenger.dto';
+import { PassengerService } from './passenger.service';
 
 @Controller('passenger')
-export class PassengerController {}
+export class PassengerController {
+    constructor(private readonly passengersService:PassengerService){}
+    @Post()
+    create(@Body() passenger:PassengerDTO){
+        return this.passengersService.create(passenger)
+    }
+}
